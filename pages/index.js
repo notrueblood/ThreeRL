32a4b9708fdffafb565331a8c09a5392f712d701
Noah Trueblood <notrueblood>
2021-02-15 17:33:05 -0500

    Add a basic scene

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,33 @@
+import { useRef, useState } from "react";
+import { Canvas } from "react-three-fiber";
+import { OrbitControls, RoundedBox } from "@react-three/drei";
+import useMeasure from "react-use-measure";
+import { ResizeObserver } from "@juggle/resize-observer";
+
 export default function Index() {
-  return <p>Hello world</p>;
+  const [ref, bounds] = useMeasure({ polyfill: ResizeObserver });
+
+  // For hover and mouse tracking.
+  const [big, setBig] = useState(false);
+  const [hovered, setHover] = useState(false);
+  const [xy, setXY] = useState([0, 0]);
+
+  return (
+    <div className="main h-screen w-screen" ref={ref}>
+      <Canvas
+        camera={{ position: [0, 0, 5] }}
+        width={bounds.width}
+        height={bounds.height}
+      >
+        <OrbitControls />
+        <RoundedBox
+          args={[1, 1, 1]} // Width, Height and Depth of the box
+          radius={0.05} // Border-Radius of the box
+          smoothness={4} // Optional, number of subdivisions
+        >
+          <meshPhongMaterial attach="material" color="#f3f3f3" wireframe />
+        </RoundedBox>
+      </Canvas>
+    </div>
+  );
 }
